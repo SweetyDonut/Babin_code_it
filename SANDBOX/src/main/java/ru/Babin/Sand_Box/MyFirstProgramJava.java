ebb0cf75c5fbc3737d2cadf67b7126e99d2d51fb
SweetyDonut <danilbabin@mail.ru>
2017-01-21 18:39:49 +0300

    Добавлено 2-е задание

--- SANDBOX/src/main/java/ru/Babin/Sand_Box/MyFirstProgramJava.java
+++ SANDBOX/src/main/java/ru/Babin/Sand_Box/MyFirstProgramJava.java
@@ -1,7 +1,22 @@
 package ru.Babin.Sand_Box;
-
 public class MyFirstProgramJava {
 	public static void main(String[] args ){
-		System.out.println("Hallo, world!!!");
+		//System.out.println("Hallo, world!!!");
+		Point p1 = new Point(2.3, 1.2);
+		Point p2 = new Point(4.5, 2.3);
+		System.out.println(distance(p1,p2));
+
+		System.out.println(p1.mDistance(p2));
+
+	}
+
+	static private double distance(Point p1, Point p2){
+
+		double x1=p1.getX();
+		double x2=p2.getX();
+		double y1=p1.getY();
+		double y2=p2.getY();
+
+		return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 	}
 }
