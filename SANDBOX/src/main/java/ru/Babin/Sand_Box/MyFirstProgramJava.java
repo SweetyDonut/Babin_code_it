369b8bcafaf9f200d08b766f7aeac17ce3904503
SweetyDonut <danilbabin@mail.ru>
2017-01-21 15:21:01 +0300

    Создан пакет для моей первой программы

--- SANDBOX/src/main/java/ru/Babin/Sand_Box/MyFirstProgramJava.java
+++ SANDBOX/src/main/java/ru/Babin/Sand_Box/MyFirstProgramJava.java
@@ -1,4 +1,6 @@
-public class MyFirstProgramJava{
+package ru.Babin.Sand_Box;
+
+public class MyFirstProgramJava {
 	public static void main(String[] args ){
 		System.out.println("Hallo, world!!!");
 	}
